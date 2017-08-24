2ab88ee9bf60dd5a024a2ba3675987dc9bc8860d
Catalin Melinte <catalin.melinte@cgm.com>
2017-08-24 15:19:18 +0300

    Fourth class added + modified FirstClass

--- src/com/cgm/firstclass/FirstClass.java
+++ src/com/cgm/firstclass/FirstClass.java
@@ -3,5 +3,10 @@ package com.cgm.firstclass;
 public class FirstClass {
 	public static void main(String[] args) {
 		System.out.println("First class-----------------");
+		System.out.println("Line added in second branch");
+		System.out.println("Line added in second branch");
+		System.out.println("Line added in second branch");
+		System.out.println("Line added in second branch");
+		System.out.println("Line added in second branch");
 	}
 }
---
+++ src/com/cgm/fourthclass/FourthClass.java
@@ -0,0 +1,7 @@
+package com.cgm.fourthclass;
+
+public class FourthClass {
+	public static void main(String[] args) {
+		System.out.println("Fourth class-----------------");
+	}
+}
